27727790d5a48daa74595971a74c4c35f2a5efc5
yousayT <yousay19981009@gmail.com>
2021-09-12 06:14:17 +0900

    add puyocamp url into Instruction.js

--- src/puyo/Instruction.js
+++ src/puyo/Instruction.js
@@ -18,7 +18,8 @@ function Instruction(props) {
               ・スタートボタンでフィバのばし開始です。<br/>
               ・一度でも消すとそれ以上ぷよを置けなくなります。<br/>
               <strong>注意</strong>：キャラやツモカスタム、れんさのタネなどの設定を変えると<strong>盤面がリセットされます。</strong><br/>
-              詳しい説明：url
+              <br/>
+              詳しい説明：<a href="https://puyo-camp.jp/posts/146537">フィーバー用のとこぷよツールを作ってみました！_ぷよぷよキャンプ</a>
             </p>
           :
             <p>
@@ -31,7 +32,8 @@ function Instruction(props) {
 
               スタートボタンでとこぷよ開始です。<br/>
               <strong>注意</strong>：キャラやツモカスタムの設定を変えると<br/><strong>盤面がリセットされます。</strong><br/>
-              詳しい説明：url
+              <br/>
+              詳しい説明：<a href="https://puyo-camp.jp/posts/146537">フィーバー用のとこぷよツールを作ってみました！_ ぷよぷよキャンプ</a>
             </p>
         }
       </div>
